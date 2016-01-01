ae7e8072dfb2b84896320b8612d4dfe562a5449b
Darijan <dvertovs@foi.hr>
2016-01-01 13:51:08 +0100

    alamofire module

---
+++ ChatApp/ws/HTTPRequest.swift
@@ -0,0 +1,36 @@
+//
+//  HTTPRequest.swift
+//  ChatApp
+//
+//  Created by MTLab on 01/01/16.
+//  Copyright © 2016 tbp. All rights reserved.
+//
+
+import Alamofire
+
+public protocol WebServiceResultDelegate
+{
+    func getResult(result: AnyObject)
+}
+
+public class HTTPRequest
+{
+    private var delegate: WebServiceResultDelegate?
+    
+    public init(delegate: WebServiceResultDelegate)
+    {
+        self.delegate = delegate
+    }
+    
+    private func httprequest(url: String, params: [String:String])
+    {
+        Alamofire.request(.POST, url, parameters: params)
+            .responseJSON { response in
+                if let json = response.result.value{
+                    
+                    self.delegate?.getResult(json)
+                    
+                }
+        }
+    }
+}
---
+++ ChatApp/ws/ws.h
@@ -0,0 +1,19 @@
+//
+//  ws.h
+//  ws
+//
+//  Created by MTLab on 01/01/16.
+//  Copyright © 2016 tbp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ws.
+FOUNDATION_EXPORT double wsVersionNumber;
+
+//! Project version string for ws.
+FOUNDATION_EXPORT const unsigned char wsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ws/PublicHeader.h>
+
+
